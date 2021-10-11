17d3c94af719616c759fdd95f81b603dc24956d1
NguyenHaoNhienITITWE20004 <91869151+NguyenHaoNhienITITWE20004@users.noreply.github.com>
2021-10-11 18:00:40 +0700

    Update Exercise3.c

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -9,30 +9,58 @@ ______________________________________
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	int testcase = atoi(argv[1]);
-	
-	//Your codes here
-		int n;
-		int i;
-	scanf("%d", &n);
-	int count;
-for (int i=2; i <= n; i++){
-	count = 0;
-}
-	while (n % i == 0){
-    ++count;
-	n /= 1;
-	}
-	if(count){
-		if (count > 1) printf("%d", i, count);
-		else printf("%d", i);
-		if(n > 1){
-			printf(" * ");
+
+	int n = atoi(argv[1]);	
+printf(" ");
+	int i;
+	int store = 3, limit = sqrt(n);
+	bool check = false;
+
+	if (n % 2 == 0)
+	{
+		printf(" 2 ");
+		n = n / 2;
+		check = true;
+		while (n % 2 == 0)
+		{
+			printf("* 2 ");
+			n = n / 2;
 		}
 	}
 
-	return 0;
+	// Now n must be odd
+	else
+	{
+		for (i = 3; i <= limit; i += 2)
+		{
+			if (n % i == 0)
+			{
+				printf(" %d ", i);
+				n = n / i;
+				store = i;
+				check = true;
+				break;
+			}
+		}
+	}
+
+	for (i = store; i <= limit; i += 2)
+	{
+
+		while (n % i == 0)
+		{
+			printf("* %d ", i);
+			n = n / i;
+		}
+	}
+
+	if (n > 2 && !check)
+		printf(" %d", n);
+	else if (n > 2 && check)
+		printf("* %d", n);
+    
+    return 0;
 }
